b7edf576620f6673d8ecfcf7cf1071030e6a537b
Ruben Guerra <70193006+Rubenthecat@users.noreply.github.com>
2021-10-02 04:27:35 -0500

    I have been commiting to the wrong repository up to this point. WHOOPS!

--- homework1/restapi.py
+++ homework1/restapi.py
@@ -1,14 +1,16 @@
 import flask
 from flask import jsonify
 from flask import request, make_response
-from sql import execute_connection
-from sql import execute_query
-from sql import execute_read_query
+
+
+#from sql import execute_connection
+#from sql import execute_query
+#from sql import execute_read_query
 
 
 #setting the application name
 app = flask.Flask(__name__) #Sets up the application
-app. confic["DEBUG"] = True #Allows errors on browser
+app. config["DEBUG"] = True #Allows errors on browser
 
 
 
@@ -23,8 +25,26 @@ app. confic["DEBUG"] = True #Allows errors on browser
 
 @app.route('/', methods=['GET']) #Contact the endpoint
 def home():
-    return "Welcome to Homework 1"
+    return "Welcome to Homework"
 
+@app.route('/api/cart', methods=['GET'])
+
+
+def cart():
+    return jsonify(items)     
+
+
+@app.route('/api/cart', methods=['GET'])
+def api_id():
+    if 'item' in request.args:
+        id = int(request.args['item'])
+    else:
+        return "NO SUCH ITEM"
+
+    results = []
+    for cart in carts:
+        if cart['item'] == item:
+            results.append(cart)
 
 
 
